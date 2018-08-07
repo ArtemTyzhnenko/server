9731358e6d6bdc1cf529e0ce04ae553cdba970a4
Artem Tyzhnenko <artem.tyznhenko@chisw.com>
2018-08-07 23:34:40 +0300

    add cors to API and change host on 3090

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:27017/auth', {useNewUrlParser: true});
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 router(app);
 
 // Server Setup
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server', port);
